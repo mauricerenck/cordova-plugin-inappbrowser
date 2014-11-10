bf57e88924f32d0e319a93aaa36bd8a3c5e9835a
Maurice Renck <maurice@maurice-renck.de>
2014-11-10 10:56:38 +0100

    ignore iframes

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -78,12 +78,6 @@ InAppBrowser.prototype = {
 };
 
 module.exports = function(strUrl, strWindowName, strWindowFeatures) {
-    // Don't catch calls that write to existing frames (e.g. named iframes).
-    if (window.frames && window.frames[strWindowName]) {
-        var origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
-        return origOpenFunc.apply(window, arguments);
-    }
-
     strUrl = urlutil.makeAbsolute(strUrl);
     var iab = new InAppBrowser();
     var cb = function(eventname) {
